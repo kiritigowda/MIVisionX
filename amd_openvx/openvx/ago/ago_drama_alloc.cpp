8573554fe89814f76bf67155f7c073a03df25824
Hansel Yang <hanselyang123@gmail.com>
2021-02-03 17:34:31 -0800

    harris fix

--- amd_openvx/openvx/ago/ago_drama_alloc.cpp
+++ amd_openvx/openvx/ago/ago_drama_alloc.cpp
@@ -187,8 +187,8 @@ int agoGpuOclAllocBuffers(AgoGraph * graph)
 			for (auto d : G) {
 				cl_mem_object_type dMemType = getMemObjectType(d);
 				if((dataMemType != dMemType) ||
-				   (s >= d->hierarchical_life_start && s <= d->hierarchical_life_end) ||
-				   (e >= d->hierarchical_life_start && e <= d->hierarchical_life_end))
+				   (s >= d->hierarchical_life_start && s <= (d->hierarchical_life_end+1)) ||
+				   ((e+1)>= d->hierarchical_life_start && (e+1) <= (d->hierarchical_life_end+1)))
 				{
 					possible = false;
 					break;
