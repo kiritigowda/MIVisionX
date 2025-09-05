b01d43f2d3f6b2266638eb7f07d9f7d9a6ecd833
kiritigowda <kiritigowda@gmail.com>
2025-09-05 10:11:33 -0700

    HIP Check - Updates

--- amd_openvx/openvx/hipvx/hip_common_funcs.h
+++ amd_openvx/openvx/hipvx/hip_common_funcs.h
@@ -29,14 +29,18 @@ THE SOFTWARE.
 #define MASK_EARLY_EXIT 4369
 #define HIPSELECT(a, b, c)  (c ? b : a)
 
-#define CHECK_HIP_RETURN_STATUS(x)                                                                         \
-    do {                                                                                               \
-        int retval = (x);                                                                              \
-        if (retval != 0) {                                                                             \
-            fprintf(stderr, "Runtime error: %s returned %d at %s:%d", #x, retval, __FILE__, __LINE__); \
-            return VX_FAILURE;                                                                         \
-        }                                                                                              \
-    } while (0)
+#define CHECK_HIP_RETURN_STATUS(expression)                \
+{                                            \
+    const hipError_t status = expression;    \
+    if(status != hipSuccess){                \
+            std::cerr << "HIP error "        \
+                << status << ": "            \
+                << hipGetErrorString(status) \
+                << " at " << __FILE__ << ":" \
+                << __LINE__ << std::endl;    \
+    }                                        \
+}
+
 #define MAX_KERNELS 500
 
 const float pi = 0x1.921fb6p+1f;
